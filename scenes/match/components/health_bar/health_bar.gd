2a52862dc04d92c025056c87dd33aab5a0f05caa
DucPham1512 <duc.phamtan@hcmut.edu.vn>
2026-06-29 19:40:33 +0700

    Implement health bar

---
+++ scenes/match/components/health_bar/health_bar.gd
@@ -0,0 +1,11 @@
+extends TextureProgressBar
+
+@export var player : Player
+
+var max_health : int = Util.one_v_one_starting_hp
+	
+func _ready() -> void:
+	player.health_changed.connect(update)
+
+func update(player_health):
+	value = player_health * 100 / max_health
